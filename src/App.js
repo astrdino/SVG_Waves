691aa912db519e0f53af070c69d2ea6a2c5fcb42
Chengxu Liang <dino.liang0609@gmail.com>
2024-05-15 21:17:53 -0700

    Update 05-15-2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  function randomColor() {
+    console.log(Math.random());
+  }
+
+  const [windowSize, setWindwoSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const [offset, setOffset] = useState(10);
+
+  //component
+
+  const Create = ({ ctrl, x, strWidth, color }) => (
+    <>
+      <line
+        x1={`${(x * windowSize.width) / 32}px`}
+        y1="0"
+        x2={`${(x * windowSize.width) / 32}px`}
+        y2={`${(14 * windowSize.height) / 32}px`}
+        stroke={color}
+        strokeWidth={strWidth}
+      ></line>
+
+      <line
+        x1={`${(x * windowSize.width) / 32}px`}
+        y1={`${(18 * windowSize.height) / 32}px`}
+        x2={`${(x * windowSize.width) / 32}px`}
+        y2={`${(32 * windowSize.height) / 32}px`}
+        stroke={color}
+        strokeWidth={strWidth}
+      ></line>
+
+      <line
+        x1={`${(x * windowSize.width) / 32}px`}
+        y1={`${(14 * windowSize.height) / 32}px`}
+        x2={`${(x * windowSize.width) / 32 + 20}px`}
+        y2={`${(14 * windowSize.height) / 32}px`}
+        stroke={color}
+        strokeWidth={strWidth}
+      ></line>
+      <line
+        x1={`${(x * windowSize.width) / 32}px`}
+        y1={`${(18 * windowSize.height) / 32}px`}
+        x2={`${(x * windowSize.width) / 32 + 20}px`}
+        y2={`${(18 * windowSize.height) / 32}px`}
+        stroke={color}
+        strokeWidth={strWidth}
+      ></line>
+      <line
+        x1={`${(x * windowSize.width) / 32 + 20}px`}
+        y1={`${(14 * windowSize.height) / 32}px`}
+        x2={`${(x * windowSize.width) / 32 + 20}px`}
+        y2={`${(18 * windowSize.height) / 32}px`}
+        stroke={color}
+        strokeWidth={strWidth}
+      ></line>
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width={`${windowSize.width}px`} height={`${windowSize.height}px`}>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <Create key={index} x={index} strWidth={"3"} color={"black"} />
+      ))}
+
+      {/* {create(1, 15)} */}
+    </svg>
   );
 }
 
